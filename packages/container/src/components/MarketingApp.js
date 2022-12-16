e4390329a7760d6c944ef9e8111bfa3c4deb343c
Balint Csikos <bcsikos@arisglobal.com>
2022-12-16 11:44:59 +0100

    Navigation with bi-direction callback comm

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,27 @@
 import { mount } from 'marketing/MarketingApp'
 import React, { useRef, useEffect } from 'react'
-
+import { useHistory } from 'react-router-dom'
 export default () => {
     const ref = useRef(null)
+    const history = useHistory()
 
     useEffect(() => {
-        mount(ref.current)
-    })
+        const {
+            // MF API upstream
+            // upstream navigation
+            onParentNavigate
+        } = mount(ref.current,
+            {
+                // MF API downstream
+                // downstream navigation
+                onNavigate: ({ pathname: nextPathname }) => {
+                    const { pathname } = history.location
+                    if (pathname !== nextPathname) {
+                        history.push(nextPathname)
+                    }
+                }
+            })
+        history.listen(onParentNavigate)
+    }, [])
     return <div ref={ref}></div>
 }
